ab4756c4a23c1d2d03194912945d0fa49a267589
SundayYogurt <kritsanalalu5@gmail.com>
2025-10-17 22:24:56 +0700

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './context/AuthContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from './App.jsx'
+import { AuthProvider } from "./context/AuthContext.jsx";
+import App from "./App.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <BrowserRouter basename="/condo-swift">
-  <AuthProvider>
-    <App/>
-    </AuthProvider>
+    <BrowserRouter basename="/condo-swift">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
