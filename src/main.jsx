a53dd8616ecf32edda6139d735b525497d26bb7b
SundayYogurt <kritsanalalu5@gmail.com>
2025-10-17 22:21:39 +0700

    add BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { BrowserRouter } from "react-router";
 import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <AuthProvider basename="/condo-swift">
+     <BrowserRouter basename="/condo-swift">
+  <AuthProvider>
     <App/>
     </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
