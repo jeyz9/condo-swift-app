0807d872fde2a8add552a677c8179ae7573ac0bd
jeyz9 <ggjj4511@gmail.com>
2026-05-26 01:51:10 +0700

    fix: map

--- src/components/details/SimpleMap.jsx
+++ src/components/details/SimpleMap.jsx
@@ -13,8 +13,13 @@ export default function SimpleMap({ lat, lng }) {
 
   const position = { lat: numericLat, lng: numericLng };
   return (
-    <div style={{ width: "100%", height: 400, borderRadius: 8, overflow: "hidden" }}>
-      <MapContainer center={position} zoom={15} style={{ width: "100%", height: "100%" }} scrollWheelZoom={true}>
+    <div className="relative z-0" style={{ width: "100%", height: 400, borderRadius: 8, overflow: "hidden" }}>
+      <MapContainer
+        center={position}
+        zoom={15}
+        style={{ width: "100%", height: "100%", zIndex: 0 }}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
